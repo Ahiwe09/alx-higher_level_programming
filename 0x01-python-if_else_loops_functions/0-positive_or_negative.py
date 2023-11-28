85afcf060b70a62b8d177a273bd8310f75e13288
Ahiwe09 <ahiwe12345@gmail.com>
2023-11-28 12:18:33 -0800

    zero_commit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print("{} ".format(number), end="")
         if number < 0:
             print("is negative")
         elif number == 0:

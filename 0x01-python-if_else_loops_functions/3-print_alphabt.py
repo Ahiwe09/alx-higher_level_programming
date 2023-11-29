e85df564ca5b94145aae20905ba12b36cc600760
Ahiwe09 <ahiwe12345@gmail.com>
2023-11-29 10:46:47 -0800

    commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,7 @@
 
 output = ""
 for i in range(97, 123):
-    if chr(i)!= "q" and chr(i) != "e":
+    if chr(i) != "q" and chr(i) != "e":
         output += chr(i)
 
 print("{:s}".format(output), end="")

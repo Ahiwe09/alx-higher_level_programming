263e135f2ed29acaed34143fdba5950f4b788f54
Ahiwe09 <ahiwe12345@gmail.com>
2023-11-29 13:41:46 -0800

    commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    output = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            output = output + chr(ord(i) - 32)
+        else:
+            output += i
+    print(output)

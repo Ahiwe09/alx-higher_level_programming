41477e9156368464b3ea2bf8e876d9dee2742fcc
Ahiwe09 <ahiwe12345@gmail.com>
2023-11-29 12:21:46 -0800

    commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 output = ""
-#built output by using string format to fill-up the strings.
+'#'built output by using string format to fill-up the strings.
 for number in range(99):
     if number < 10:
         output += "0{:d}, ".format(number)
     else:
         output += "{:d}, ".format(number)
-#print from 0 to 98.
-print("{:s}".format(output),end="")
-#and print 99.
+'#'print from 0 to 98.
+print("{:s}".format(output), end="")
+'#'and print 99.
 print("{:d}".format(99))
 

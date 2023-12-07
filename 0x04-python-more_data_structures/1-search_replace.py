2b08d8da2592170ed3523043391770eae5e9b634
Ahiwe09 <ahiwe12345@gmail.com>
2023-12-07 10:06:38 -0800

    first_commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    output = []
+    for item in my_list:
+        if item == search:
+            output.append(replace)
+        else:
+            output.append(item)
+    return(output)
